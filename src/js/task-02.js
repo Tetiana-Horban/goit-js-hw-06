6b4548d0d82f1c6b4754b1fd1a40e98c9d9a0a79
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-10-12 14:51:41 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listEl = document.querySelector('#ingredients');
+
+const makeIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.classList.add('item');
+    itemEl.textContent = ingredient;
+
+    return itemEl;
+  });
+};
+const items = makeIngredients(ingredients);
+listEl.append(...items);
