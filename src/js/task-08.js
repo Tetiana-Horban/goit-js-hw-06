d568ecab74199596dadb69832909b7fcc86c7b98
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-10-15 15:11:12 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
+const btnEl = document.querySelector('button');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((email, password) => {
+    if (email === '' || password === '') {
+      alert('Все поля должны быть заполнены!');
+    }
+    console.log(...formData);
+    formEl.email.value = '';
+    formEl.password.value = '';
+  });
+}
