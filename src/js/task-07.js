c7e796045ad81ed6af0a1ce38b91ef076aec10ba
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-10-15 17:25:25 +0300

    Delete task-07.js

--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const inputEl = document.querySelector('#font-size-control');
-const spanEl = document.querySelector('#text');
-
-inputEl.addEventListener('change', changeSizeText);
-function changeSizeText() {
-  spanEl.style.fontSize = inputEl.value + 'px';
-}
