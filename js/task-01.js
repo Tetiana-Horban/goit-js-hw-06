24d7272dc9f67cdd3f164aa51f0dbaa997e6a258
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-10-16 19:48:04 +0300

    Corect task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,11 @@
 const arrayListElement = document.querySelectorAll('.item');
 const numberListElement = arrayListElement.length;
 console.log(`Number of categories: `, numberListElement);
-
-const arrayTitleElementCategory = [...arrayListElement].map(
-  categories => categories.children[0].textContent,
+arrayListElement.forEach(element =>
+  console.log(
+    `Category: `,
+    element.children[0].textContent,
+    `Elements: `,
+    element.children[1].children.length,
+  ),
 );
-const numberItemElement = [...arrayListElement].map(
-  categories => categories.children[1].children.length,
-);
-
-console.log(`Category: `, arrayTitleElementCategory[0]);
-console.log(`Elements:`, numberItemElement[0]);
-console.log(`Category: `, arrayTitleElementCategory[1]);
-console.log(`Elements:`, numberItemElement[1]);
-console.log(`Category: `, arrayTitleElementCategory[2]);
-console.log(`Elements:`, numberItemElement[2]);
