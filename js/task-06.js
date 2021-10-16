09b339a0bce9acd5e0efffcd5f372f75e2a09f01
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-10-16 20:17:42 +0300

    Corect task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,15 +2,10 @@ const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur', onValidationInput);
 const validValue = inputEl.dataset.length;
-
 function onValidationInput(event) {
-  if (event.currentTarget.value.length === 0) {
-    inputEl.classList.remove('invalid');
-    inputEl.classList.remove('valid');
-  } else if (event.currentTarget.value.length < validValue) {
+  if (event.currentTarget.value.length != validValue) {
     inputEl.classList.add('invalid');
   } else {
-    inputEl.classList.remove('invalid');
-    inputEl.classList.add('valid');
+    inputEl.classList.replace('invalid', 'valid');
   }
 }
