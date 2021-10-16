c781fe7fb73c3e9e50cedf28597e153a181bdf98
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-10-16 20:42:11 +0300

    Corect task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
+const spanEl = document.querySelector('.color');
 const btnEl = document.querySelector('.change-color');
 const bodyEl = document.querySelector('body');
 
 btnEl.addEventListener('click', changeBackroundcolor);
 
 function changeBackroundcolor() {
+  spanEl.textContent = getRandomHexColor();
   bodyEl.style.backgroundColor = getRandomHexColor();
 }
