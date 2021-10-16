aad3fa71bfbb9f7514eb0eaac50091d35e842795
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-10-16 20:27:32 +0300

    Corect task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputEl = document.querySelector('#font-size-control');
 const spanEl = document.querySelector('#text');
 
+spanEl.style.fontSize = inputEl.value + 'px';
+
 inputEl.addEventListener('change', changeSizeText);
 function changeSizeText() {
   spanEl.style.fontSize = inputEl.value + 'px';
