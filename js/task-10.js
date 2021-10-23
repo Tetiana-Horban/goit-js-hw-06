0da7442006933e6c50c99c950047044fbbf4e298
Tetiana-Horban <tanya.ryptyk@gmail.com>
2021-10-23 14:42:18 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,28 +3,26 @@ function getRandomHexColor() {
 }
 
 const conteinerEl = document.querySelector('#boxes');
-const createBtnEl = document.querySelector('button[data-action="create"]');
-const destroyBtnEl = document.querySelector('button[data-action="destroy"]');
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
 const inputEl = document.querySelector('input');
 
 inputEl.addEventListener('input', createIndexInput);
 createBtnEl.addEventListener('click', createBoxes);
 destroyBtnEl.addEventListener('click', deleteBoxes);
 
-function createIndexInput(element) {
-  inputEl.setAttribute('counter', Number(element.currentTarget.value));
+function createIndexInput() {
+  inputEl.value;
 }
 
 function createBoxes() {
   let sizeBox = 30;
-  let amount = Number(inputEl.getAttribute('counter'));
-
+  let amount = Number(inputEl.value);
   for (let i = 0; i < amount; i += 1) {
     sizeBox += 10;
     const divEl = document.createElement('div');
 
-    divEl.style.cssText =
-      'width: ${sizeBox}px; height: ${sizeBox}px; background-color: ${getRandomHexColor()}';
+    divEl.style.cssText = `width: ${sizeBox}px; height: ${sizeBox}px; background-color: ${getRandomHexColor()}; display: flex; margin: 10px`;
 
     conteinerEl.appendChild(divEl);
 
